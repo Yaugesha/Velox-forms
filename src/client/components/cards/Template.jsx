6256279157abe4f590a3c5c2968934966949e0e2
yaugesha <eugene.korchemenko@gmail.com>
2023-09-14 23:32:16 +0300

    feat: add templates gallery page layout

--- src/client/components/cards/Template.jsx
+++ src/client/components/cards/Template.jsx
@@ -1,11 +1,12 @@
 function Template({ picture, title, description, link }) {
+  console.log(description);
   return (
     <div>
-      <div className="flex justify-center items-center mb-2.5 w-[144px] h-[186px] border-[1px] border-[#dadce0]">
+      <div className="flex justify-center items-center mb-2.5 w-[167px] h-[216px] border-[1px] border-[#dadce0]">
         <img src={picture} alt="template photo" />
       </div>
-      <span className="font-medium">{title}</span>
-      <span>{description}</span>
+      <div className="font-medium">{title}</div>
+      <div className="text-[#5f6368]">{description}</div>
     </div>
   );
 }
---
+++ src/client/pages/TemplaytesGaliery.jsx
@@ -0,0 +1,142 @@
+import { useRef } from "react";
+import Template from "../components/cards/Template";
+
+function TemplaytesGaliery() {
+  const templatesArr = useRef([
+    {
+      type: "Favorite",
+      templaytes: [
+        {
+          title: "New document1",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document2",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document3",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document4",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document5",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+      ],
+    },
+    {
+      type: "Popular1",
+      templaytes: [
+        {
+          title: "New document1",
+          description: "description",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document",
+          description: "description",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document2",
+          description: "description",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+      ],
+    },
+    {
+      type: "Popular2",
+      templaytes: [
+        {
+          title: "New document1",
+          description: "description",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+      ],
+    },
+    {
+      type: "Popular3",
+      templaytes: [
+        {
+          title: "New document1",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document2",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document3",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document4",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document5",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document6",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+        {
+          title: "New document7",
+          picture: "src/client/assets/icons/tamplates/icon-plus.svg",
+        },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <header className="flex  border-b-2 border-solid border-black mb-6">
+        <div className="flex items-center text-base leading-6 ">
+          <div className="inline-block items-center w-12 cursor-pointer mr-5 ml-3 p-3 font-bold leading-7 text-2xl text-[#5f6368] font-serif">
+            &larr;
+          </div>
+          <span className="mr-2">Templates gallery</span>
+        </div>
+      </header>
+      <main>
+        <div className="w-[980px] flex flex-col justify-between gap-8">
+          {templatesArr.current.map((template) => {
+            return (
+              <div key={template.type}>
+                <h4 className="flex mb-5 ml-2 leading-6 text-base">
+                  {template.type}
+                </h4>
+                <div className="flex flex-wrap gap-5">
+                  {template.templaytes.map((template) => {
+                    return (
+                      <Template
+                        title={template.title}
+                        description={template.description}
+                        picture={template.picture}
+                        key={template.title}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default TemplaytesGaliery;
