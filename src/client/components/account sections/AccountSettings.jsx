b7ea6cf5cd264aef1beda55dfdeb0acd95cbe710
yaugesha <eugene.korchemenko@gmail.com>
2023-09-16 10:45:02 +0300

    fix: change id for inputs and labels in Acount settings

--- src/client/components/account sections/AccountSettings.jsx
+++ src/client/components/account sections/AccountSettings.jsx
@@ -17,22 +17,49 @@ function AccountSettings() {
           Update
         </div>
       </div>
-      <div className="w-[590px] flex flex-wrap gap-[18px]">
-        <form className="w-[590px] flex flex-wrap gap-[18px]" action="">
-          <Input placeholder={"Full name"} width={"286px"} />
-          <Input placeholder={"Email Address"} width={"286px"} />
-          <Input placeholder={"Adress"} width={"590px"} />
-          <Input placeholder={"City"} width={"286px"} />
-          <Input placeholder={"State/Province"} width={"286px"} />
-          <Input placeholder={"Zip Code"} width={"286px"} />
-          <Input placeholder={"Country"} width={"286px"} />
-          <Input placeholder={"Phone number"} width={"286px"} />
-          <Input placeholder={"Date of birth"} width={"286px"} />
-          <Input placeholder={"Adress"} width={"590px"} />
-          <Input placeholder={"Office address"} width={"286px"} />
-          <Input placeholder={"Place of work "} width={"286px"} />
-          <Input placeholder={"Work Zip Code"} width={"286px"} />
-          <Input placeholder={"Passport No."} width={"286px"} />
+      <div>
+        <form
+          className="w-[590px] flex flex-wrap justify-between gap-[18px]"
+          action=""
+        >
+          <Input placeholder={"Full name"} width={"286px"} id={"fullName"} />
+          <Input placeholder={"Email Address"} width={"286px"} id={"email"} />
+          <Input placeholder={"Adress"} width={"590px"} id={"adress"} />
+          <Input placeholder={"City"} width={"286px"} id={"ciyu"} />
+          <Input placeholder={"State/Province"} width={"286px"} id={"state"} />
+          <Input placeholder={"Zip Code"} width={"286px"} id={"zipCode"} />
+          <Input placeholder={"Country"} width={"286px"} id={"country"} />
+          <Input
+            placeholder={"Phone number"}
+            width={"286px"}
+            id={"phoneNumber"}
+          />
+          <Input
+            placeholder={"Date of birth"}
+            width={"286px"}
+            id={"dateOfBirth"}
+          />
+          <Input placeholder={"Adress"} width={"590px"} id={"adress"} />
+          <Input
+            placeholder={"Office address"}
+            width={"286px"}
+            id={"offiveAdress"}
+          />
+          <Input
+            placeholder={"Place of work "}
+            width={"286px"}
+            id={"placeOfWork"}
+          />
+          <Input
+            placeholder={"Work Zip Code"}
+            width={"286px"}
+            id={"workZipCode"}
+          />
+          <Input
+            placeholder={"Passport No."}
+            width={"286px"}
+            id={"passwordNo"}
+          />
           <button className="w-[590px] h-[45px] bg-black text-sm text-white flex items-center justify-center">
             UPDATE INFORMATION
           </button>
--- src/client/components/account sections/Input.jsx
+++ src/client/components/account sections/Input.jsx
@@ -1,14 +1,14 @@
-function Input({ placeholder, width }) {
+function Input({ placeholder, width, id }) {
   return (
     <div className="flex flex-col gap-2">
-      <label className="text-xs tracking[5%]" htmlFor="#fullName">
+      <label className="text-xs tracking[5%]" htmlFor={`#${id}`}>
         {placeholder}
       </label>
       <input
         className={`w-[${width}] h-[45px] border-2 border-black pl-4`}
         placeholder={placeholder}
         id="fullName"
-        type="text"
+        type={`#${id}`}
       />
     </div>
   );
