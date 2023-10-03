fe04f53474c659ba4cd5bb4aa3e028c4351fbbb6
yaugesha <eugene.korchemenko@gmail.com>
2023-10-03 22:06:51 +0300

    refactor: add icons to table buttons

--- src/client/components/text-editor/TableButtons.jsx
+++ src/client/components/text-editor/TableButtons.jsx
@@ -1,6 +1,10 @@
 function TableButtons({ editor }) {
+
+  const buttonClass =
+  "flex items-center justify-center w-12 h-8 border-[1px] border-black";
+
   return (
-    <div>
+    <div className="flex flex-wrap">
       <button
         onClick={() =>
           editor
@@ -9,47 +13,35 @@ function TableButtons({ editor }) {
             .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
             .run()
         }
-      >
-        crT
+        className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-create-table.svg' alt="create table"/>
       </button>
-      <button onClick={() => editor.chain().focus().addColumnBefore().run()}>
-        addColumnBefore
+      <button onClick={() => editor.chain().focus().addColumnBefore().run()} className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-add-column-before.svg' alt="add col bef"/>
       </button>
-      <button onClick={() => editor.chain().focus().addColumnAfter().run()}>
-        addColumnAfter
+      <button onClick={() => editor.chain().focus().addColumnAfter().run()} className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-add-column-after.svg' alt="add col aft"/>
       </button>
-      <button onClick={() => editor.chain().focus().deleteColumn().run()}>
-        deleteColumn
+      <button onClick={() => editor.chain().focus().deleteColumn().run()}className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-delete-column.svg' alt="del col"/>
       </button>
-      <button onClick={() => editor.chain().focus().addRowBefore().run()}>
-        addRowBefore
+      <button onClick={() => editor.chain().focus().addRowBefore().run()} className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-add-row-before.svg' alt="add row bef"/>
       </button>
-      <button onClick={() => editor.chain().focus().addRowAfter().run()}>
-        addRowAfter
+      <button onClick={() => editor.chain().focus().addRowAfter().run()} className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-add-row-after.svg' alt="add row af"/>
       </button>
-      <button onClick={() => editor.chain().focus().deleteRow().run()}>
-        deleteRow
+      <button onClick={() => editor.chain().focus().deleteRow().run()} className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-delete-row.svg' alt="del row"/>
       </button>
-      <button onClick={() => editor.chain().focus().deleteTable().run()}>
-        deleteTable
+      <button onClick={() => editor.chain().focus().deleteTable().run()} className={buttonClass}>
+        <img src='/src/client/assets/icons/text-editor/icon-delete-table.svg' alt="delete table"/>
       </button>
-      <button onClick={() => editor.chain().focus().mergeCells().run()}>
+      {/* <button onClick={() => editor.chain().focus().mergeCells().run()}>
         mergeCells
       </button>
       <button onClick={() => editor.chain().focus().splitCell().run()}>
         splitCell
-      </button>
-      <button onClick={() => editor.chain().focus().toggleHeaderColumn().run()}>
-        toggleHeaderColumn
-      </button>
-      <button onClick={() => editor.chain().focus().toggleHeaderRow().run()}>
-        toggleHeaderRow
-      </button>
-      <button onClick={() => editor.chain().focus().toggleHeaderCell().run()}>
-        toggleHeaderCell
-      </button>
-      <button onClick={() => editor.chain().focus().mergeOrSplit().run()}>
-        mergeOrSplit
       </button>
       <button
         onClick={() =>
@@ -60,13 +52,7 @@ function TableButtons({ editor }) {
       </button>
       <button onClick={() => editor.chain().focus().fixTables().run()}>
         fixTables
-      </button>
-      <button onClick={() => editor.chain().focus().goToNextCell().run()}>
-        goToNextCell
-      </button>
-      <button onClick={() => editor.chain().focus().goToPreviousCell().run()}>
-        goToPreviousCell
-      </button>
+      </button> */}
     </div>
   );
 }
