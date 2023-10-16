92ca239c5e31bbbb040451b793dbbda712f08291
Yaugesha <eugene.korchemenko@gmail.com>
2023-10-17 00:03:57 +0300

    feat: connect new node with its input

--- src/client/components/document-fields/InputFields.jsx
+++ src/client/components/document-fields/InputFields.jsx
@@ -1,6 +1,13 @@
 import Input from "../account sections/Input";
 
 function InputFields({ fields, removeField }) {
+  function generateClassName(inputString) {
+    const cleanedString = inputString.replace(/\W+/g, "-").toLowerCase();
+    const prefix = "custom-class-";
+    const className = `${prefix}${cleanedString}`;
+    return className;
+  }
+
   const findLabel = (input) => {
     const labels = document.querySelectorAll("label");
     for (let i = 0; i < labels.length; i++) {
@@ -11,35 +18,23 @@ function InputFields({ fields, removeField }) {
   };
 
   function changeInputSelecors(field, input, label) {
-    const clas = input.value;
+    const selector = generateClassName(input.value);
+    console.log(field);
     field.classList.remove(input.id);
     if (field == undefined || input.value == "") {
-      document.querySelector(".editor").querySelector(`p`).removeChild(field);
+      document
+        .querySelector(".editor")
+        .querySelector(`p`)
+        .removeChild(field.parentNode);
       label.remove();
       input.remove();
       removeField(input.id);
     } else {
-      // if (input.value.includes(" ")) {
-      //   const identificator = input.value.split(" ").join("-");
-      //   input.id = identificator;
-      //   field.classList.add(identificator);
-      //   label.htmlFor = identificator;
-      // } else {
-      input.id = hash; //input.value;
-      field.classList.add(hash); //input.value);
-      label.htmlFor = hash; //input.value;
-      //}
+      input.id = selector;
+      field.classList.add(selector);
+      label.htmlFor = selector;
+      field.innerText = input.value;
     }
-  }
-
-  function alphanumericInput(input, label, field) {
-    let letters = /^[0-9a-zA-Z]+$/;
-    if (input.value.match(letters) || input.value == "" || input.value == " ") {
-      changeInputSelecors(field, input, label);
-      label.innerText = input.value;
-      input.placeholder = input.value;
-      if (input.value !== "") field.innerText = input.value;
-    } else input.value = input.value.slice(0, -1);
   }
 
   function handleInput(e) {
@@ -47,7 +42,6 @@ function InputFields({ fields, removeField }) {
     const label = findLabel(input);
     const field = document.querySelector(`.${input.id}`);
     changeInputSelecors(field, input, label);
-    //alphanumericInput(input, label, field);
   }
 
   return (
